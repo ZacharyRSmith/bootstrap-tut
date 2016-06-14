f4270c758b123bb080b0d47718e64dc7672fece0
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-06-14 17:03:56 -0400

    enhance: impl ease scrolling.

--- app/scripts/scrolling.js
+++ app/scripts/scrolling.js
@@ -4,6 +4,15 @@ $(function () {
   });
 
   $('body').scrollspy({
-    target: '.navbar'
-  })
+    target: '.navbar',
+    offset: 130
+  });
+
+  $('nav a, .down-button a').bind('click', function () {
+    // animate both html and body to cover all browsers
+    $('html, body').stop().animate({
+      scrollTop: $($(this).attr('href')).offset().top - 100
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+  });
 });
