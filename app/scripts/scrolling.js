4f1f7756a48ae95d2dd6ac206efa1822846ac8e9
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-06-14 16:50:46 -0400

    feat: set up homepage links.

--- app/scripts/scrolling.js
+++ app/scripts/scrolling.js
@@ -2,4 +2,8 @@ $(function () {
   $(window).on("load resize", function () {
     $(".fill-screen").css("height", window.innerHeight);
   });
+
+  $('body').scrollspy({
+    target: '.navbar'
+  })
 });
